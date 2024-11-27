223f3e66a589fe1fd1d153dc92546a3afa31031a
jireh018 <jirehtuto71@gmail.com>
2024-11-26 19:09:10 -0500

    redux toolkit for managing mutliple slices

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const carsSlice = createSlice({
+  name: 'cars',
+  initialState: {
+    searchTerm: '',
+    data: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      //assumption:
+      //action.payload === { name: 'ab', cost: 140 }
+      state.data.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    },
+    removeCar(state, action) {
+      //assumption:
+      //action.payload === the id of the car to remove
+      const updatedCars = state.data.filter((cars) => {
+        return cars.id !== action.payload;
+      });
+      state.data = updatedCars;
+    },
+  },
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
